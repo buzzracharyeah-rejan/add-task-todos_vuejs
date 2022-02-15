18bf26320d7c40e35c30daace475ef2088837d97
rejan buzzracharyeah <rejandev@gmail.com>
2022-02-15 11:13:49 +0545

    created some components for tests

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <h4>footer</h4>
+    <p>{{text}}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1.25rem;
+  gap: 1rem;
+}
+</style>
