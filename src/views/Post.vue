fb4d9184a80cc5d4db28cfed8dcdf5e91dd8a1bc
rejan buzzracharyeah <rejandev@gmail.com>
2022-03-04 17:27:15 +0545

    vue-router basics

---
+++ src/views/Post.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Post component </h1>
+</template>
+
+<script>
+export default {
+    name: "Post"
+}
+</script>
---
+++ src/views/PostDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>post detail component</h1>
+</template>
+
+<script>
+export default {
+  name: 'PostDetail',
+};
+</script>
